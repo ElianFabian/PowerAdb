512c6145fafc481fc9f4515542e998f3ea3eeaa8
Elián Fabián <elian.fabian.2001@gmail.com>
2024-09-11 01:29:34 +0200

    Fix/Improve Adb-GetSystemLanguage
    - Added missing process block
    - Added support for +16 API level

--- Public/Get-AdbSystemLocale.ps1
+++ Public/Get-AdbSystemLocale.ps1
@@ -7,23 +7,48 @@ function Get-AdbSystemLocale {
         [string[]] $DeviceId
     )
 
-    foreach ($id in $DeviceId) {
-        $apiLevel = Get-AdbApiLevel -DeviceId $id
-        if ($apiLevel -ge 24) {
-            $systemLocaleList = Get-AdbSetting -DeviceId $id -Namespace System -key 'system_locales'
-            if ($systemLocaleList -like '*null*') {
-                Get-AdbProp -DeviceId $id -PropertyName ro.product.locale
+    process {
+        foreach ($id in $DeviceId) {
+            $apiLevel = Get-AdbApiLevel -DeviceId $id
+            if (-not $apiLevel) {
+                Write-Error "Failed to retrieve the API level for device with id '$id'."
                 continue
             }
 
-            $systemLocaleList.Split(',')
-        }
-        else {
-            $systemLocale = Get-AdbProp -DeviceId $id -PropertyName persist.sys.locale
-            if ($systemLocale -cmatch "^[a-z]{2}-[A-Z]{2}$") {
-                $systemLocale
-            } else {
-                Get-AdbProp -DeviceId $id -PropertyName ro.product.locale
+            if ($apiLevel -ge 24) {
+                $systemLocaleList = Get-AdbSetting -DeviceId $id -Namespace System -Key 'system_locales'
+                if ($systemLocaleList -like '*null*') {
+                    Get-AdbProp -DeviceId $id -PropertyName 'ro.product.locale'
+                }
+                else {
+                    $systemLocaleList.Split(',')
+                }
+            }
+            elseif ($apiLevel -ge 23) {
+                $systemLocale = Get-AdbProp -DeviceId $id -PropertyName 'persist.sys.locale'
+                if ($systemLocale -cmatch "^[a-z]{2}-[A-Z]{2}$") {
+                    $systemLocale
+                }
+                else {
+                    Get-AdbProp -DeviceId $id -PropertyName 'ro.product.locale'
+                }
+            }
+            elseif ($apiLevel -ge 16) {
+                $systemLanguage = Get-AdbProp -DeviceId $id -PropertyName 'persist.sys.language'
+                if ($systemLanguage) {
+                    $systemCountry = Get-AdbProp -DeviceId $id -PropertyName 'persist.sys.country'
+
+                    "$systemLanguage-$systemCountry"
+                }
+                else {
+                    $defaultSystemLanguage = Get-AdbProp -DeviceId $id -PropertyName 'ro.product.locale.language'
+                    $defaultSystemCountry = Get-AdbProp -DeviceId $id -PropertyName 'ro.product.locale.country'
+
+                    "$defaultSystemLanguage-$defaultSystemCountry"
+                }
+            }
+            else {
+                Write-Error "Unsupported API level: '$apiLevel'. Only API levels 16 and above are supported."
             }
         }
     }
