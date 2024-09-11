fa7a58407f8f1b59ceb687b95c1bf8b62439403e
Elián Fabián <elian.fabian.2001@gmail.com>
2024-09-12 00:15:01 +0200

    Add Start-AdbProcessDeath

---
+++ Public/Start-AdbProcessDeath.ps1
@@ -0,0 +1,20 @@
+# Frees up app memory if possible
+function Start-AdbProcessDeath {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [string[]] $DeviceId,
+
+        [Parameter(Mandatory)]
+        [string[]] $ApplicationId
+    )
+
+    process {
+        foreach ($id in $DeviceId) {
+            foreach ($appId in $ApplicationId) {
+                Invoke-AdbExpression -DeviceId $id -Command "shell am kill '$appId'"
+            }
+        }
+    }
+}
