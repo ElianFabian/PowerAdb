3512279755d20cc2bc039539f18187f10d2b849d
Elián Fabián <elian.fabian.2001@gmail.com>
2024-09-12 00:47:48 +0200

    Remove adb parameter in Invoke-AdbDeepLink
    Actually I didn't see any different behavior when using any parameter

--- Public/Invoke-AdbDeepLink.ps1
+++ Public/Invoke-AdbDeepLink.ps1
@@ -14,8 +14,7 @@ function Invoke-AdbDeepLink {
 
     process {
         # TODO: maybe we could make this more flexible or maybe it doesn't make sense to use
-        # the '--activity-single-top' in every case (it's here because it allows to always open the deep-link)
-        # Or even we could define a more generic function that allows to start any kind of component with a custom intent
-        $DeviceId | Invoke-AdbExpression -Command "shell am start -a android.intent.action.VIEW -d '$Uri' '$ApplicationId' --activity-single-top" > $null
+        # Or even we could define a more generic way to use Activity Manager
+        $DeviceId | Invoke-AdbExpression -Command "shell am start -a android.intent.action.VIEW -d '$Uri' '$ApplicationId" > $null
     }
 }
