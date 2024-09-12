0a71d3c11e805fe80e25f7725bea1aa8cfdf4238
Elián Fabián <elian.fabian.2001@gmail.com>
2024-09-13 00:06:54 +0200

    Refactor Invoke-AdbExpression

--- Public/Invoke-AdbExpression.ps1
+++ Public/Invoke-AdbExpression.ps1
@@ -73,12 +73,12 @@ public static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint lpMode);
     $script:Kernel32 = Add-Type -MemberDefinition $methodDefinitions -Name 'Kernel32' -Namespace 'Win32' -PassThru -Verbose:$false
     $script:HConsoleHandle = $script:Kernel32::GetStdHandle(-11) # STD_OUTPUT_HANDLE
     $script:ConsoleMode = 7
-}
 
-# At some point in some PowerShell Core version for Windows there are problems related to ANSI
-# In our case the first verbose message when looping a collection it looks like '[33;1mVERBOSE'
-# Check this link for hardly more information and the solution used here: https://stackoverflow.com/a/70534568/18418162
-# This function doesn't seem to have any performance issue
-function ResetConsoleColors {
-    $script:Kernel32::SetConsoleMode($script:HConsoleHandle, $script:ConsoleMode) > $null
+    # At some point in some PowerShell Core version for Windows there are problems related to ANSI
+    # In our case the first verbose message when looping a collection it looks like '[33;1mVERBOSE'
+    # Check this link for hardly more information and the solution used here: https://stackoverflow.com/a/70534568/18418162
+    # This function doesn't seem to have any performance overhead
+    function ResetConsoleColors {
+        $script:Kernel32::SetConsoleMode($script:HConsoleHandle, $script:ConsoleMode) > $null
+    }
 }
