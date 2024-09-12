6fb36fbc7361dd49ec5b809c1200da8837bb1a32
Elián Fabián <elian.fabian.2001@gmail.com>
2024-09-12 23:22:54 +0200

    Fix Invoke-AdbExpression verbose output for some PowerShell Core versions for Windows
    Check the code for more information

--- Public/Invoke-AdbExpression.ps1
+++ Public/Invoke-AdbExpression.ps1
@@ -22,6 +22,11 @@ function Invoke-AdbExpression {
         if ($stopExecution) {
             return
         }
+
+        if ($isWindows -and $IsCoreCLR) {
+            ResetConsoleColors
+        }
+
         if (-not $PSBoundParameters.ContainsKey('DeviceId')) {
             if ($availableDevicesCount -gt 1) {
                 Write-Error "There are multiple devices connected, you have to indicate the device id"
@@ -34,16 +39,16 @@ function Invoke-AdbExpression {
             [System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
             if (-not $DeviceId) {
-                if ($PSCmdlet.ShouldProcess("adb $Command")) {
-                    Write-Verbose "adb $Command"
-                    Invoke-Expression "adb $Command"
+                $adbCommand = "adb $Command"
+                if ($PSCmdlet.ShouldProcess($adbCommand)) {
+                    Invoke-Expression $adbCommand
                 }
             }
             else {
-                $DeviceId | ForEach-Object {
-                    if ($PSCmdlet.ShouldProcess("adb -s $_ $Command")) {
-                        Write-Verbose "adb -s $_ $Command"
-                        Invoke-Expression "adb -s $_ $Command"
+                foreach ($id in $DeviceId) {
+                    $adbCommand = "adb -s $id $Command"
+                    if ($PSCmdlet.ShouldProcess($adbCommand)) {
+                        Invoke-Expression $adbCommand
                     }
                 }
             }
@@ -52,4 +57,28 @@ function Invoke-AdbExpression {
             [System.Console]::OutputEncoding = $previousEncoding
         }
     }
+}
+
+
+
+if ($IsWindows -and $IsCoreCLR) {
+    $methodDefinitions = @'
+[DllImport("kernel32.dll", SetLastError = true)]
+public static extern IntPtr GetStdHandle(int nStdHandle);
+[DllImport("kernel32.dll", SetLastError = true)]
+public static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+[DllImport("kernel32.dll", SetLastError = true)]
+public static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint lpMode);
+'@
+    $script:Kernel32 = Add-Type -MemberDefinition $methodDefinitions -Name 'Kernel32' -Namespace 'Win32' -PassThru -Verbose:$false
+    $script:HConsoleHandle = $script:Kernel32::GetStdHandle(-11) # STD_OUTPUT_HANDLE
+    $script:ConsoleMode = 7
+}
+
+# At some point in some PowerShell Core version for Windows there are problems related to ANSI
+# In our case the first verbose message when looping a collection it looks like '[33;1mVERBOSE'
+# Check this link for hardly more information and the solution used here: https://stackoverflow.com/a/70534568/18418162
+# This function doesn't seem to have any performance issue
+function ResetConsoleColors {
+    $script:Kernel32::SetConsoleMode($script:HConsoleHandle, $script:ConsoleMode) > $null
 }
