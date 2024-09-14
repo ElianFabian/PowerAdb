a7a03c47c0a166dff8fc05b70d93ea3f6dd0c7f4
Elián Fabián <elian.fabian.2001@gmail.com>
2024-09-14 03:46:54 +0200

    Fix Get-AdbSetting verbose
    For some reason in some cases $Verbose is not inherited and you have to manually use $VerbosePreference

--- Public/Get-AdbSetting.ps1
+++ Public/Get-AdbSetting.ps1
@@ -55,7 +55,7 @@ function Get-AdbSetting {
                 continue
             }
 
-            Invoke-AdbExpression -DeviceId $id -Command "shell settings get $namespaceLowercase $Key" `
+            Invoke-AdbExpression -DeviceId $id -Command "shell settings get $namespaceLowercase $Key" -Verbose:$VerbosePreference `
             | Out-String -Stream `
             | Where-Object {
                 -not [string]::IsNullOrWhiteSpace($_)
