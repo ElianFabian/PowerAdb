2571b3f7c48449b93d8687ceb4565747d213ac86
Elián Fabián <elian.fabian.2001@gmail.com>
2024-10-20 13:13:13 +0200

    Add Enable/Disable functions for Wi-Fi

---
+++ Public/Disable-AdbWifi.ps1
@@ -0,0 +1,20 @@
+function Disable-AdbWifi {
+    
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [string[]] $DeviceId
+    )
+
+    process {
+        foreach ($id in $DeviceId) {
+            $apiLevel = Get-AdbApiLevel -DeviceId $id -Verbose:$false
+            if ($apiLevel -lt 30) {
+                Write-Error "Enable wifi is not supported for device with id '$id' with API level of '$apiLevel'. Only API levels 30 and above are supported."
+                continue
+            }
+
+            Invoke-AdbExpression -DeviceId $id -Command "shell cmd -w wifi set-wifi-enabled disabled" -Verbose:$VerbosePreference
+        }
+    }
+}
