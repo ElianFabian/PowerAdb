b55ece19f6f37de91804f87af09132f135957a12
Elián Fabián <elian.fabian.2001@gmail.com>
2024-10-27 14:01:35 +0100

    Add documentation link in Test-AdbAppHasFlag

--- Private/Test-AdbAppHasFlag.ps1
+++ Private/Test-AdbAppHasFlag.ps1
@@ -16,6 +16,7 @@ function Test-AdbAppHasFlag {
     process {
         foreach ($id in $DeviceId) {
             foreach ($appId in $ApplicationId) {
+                # For more information: https://developer.android.com/guide/topics/manifest/application-element
                 Invoke-AdbExpression -DeviceId $id -Command "shell dumpsys package '$appId'" `
                 | Select-String -Pattern "flags=\[\s.+\s\]" `
                 | Select-Object -ExpandProperty Matches -First 1 `
