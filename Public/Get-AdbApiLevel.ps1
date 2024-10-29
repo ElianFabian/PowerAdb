41018557e7829597a48031a0be05d25ba2fece1a
Elián Fabián <elian.fabian.2001@gmail.com>
2024-10-29 16:53:18 +0100

    Refactor Get-AdbApiLevel

--- Public/Get-AdbApiLevel.ps1
+++ Public/Get-AdbApiLevel.ps1
@@ -8,14 +8,15 @@ function Get-AdbApiLevel {
     )
 
     process {
-        $DeviceId | ForEach-Object {
-            $cache = Get-CacheValue -DeviceId $_ -ErrorAction SilentlyContinue
+        foreach ($id in $DeviceId) {
+            $cache = Get-CacheValue -DeviceId $id -ErrorAction SilentlyContinue
             if ($null -ne $cache) {
+                Write-Verbose "Get cached API level: '$cache'"
                 [uint32] $cache
             }
             else {
-                $apiLevel = Get-AdbProperty -DeviceId $_ -Name 'ro.build.version.sdk' -QueryFromList -Verbose:$VerbosePreference 2> $null
-                Set-CacheValue -DeviceId $_ -Value $apiLevel
+                $apiLevel = Get-AdbProperty -DeviceId $id -Name 'ro.build.version.sdk' -Verbose:$VerbosePreference 2> $null
+                Set-CacheValue -DeviceId $id -Value $apiLevel
                 [uint32] $apiLevel
             }
         }
