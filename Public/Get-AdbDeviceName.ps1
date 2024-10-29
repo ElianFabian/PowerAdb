5acf99eb29e3e00e3c324e11983aed154d7d247b
Elián Fabián <elian.fabian.2001@gmail.com>
2024-10-29 16:53:35 +0100

    Add caching to Get-AdbDeviceName

--- Public/Get-AdbDeviceName.ps1
+++ Public/Get-AdbDeviceName.ps1
@@ -9,11 +9,23 @@ function Get-AdbDeviceName {
 
     process {
         foreach ($id in $DeviceId) {
-            if ((Test-AdbEmulator -DeviceId $id)) {
-                [string] (Invoke-AdbExpression -DeviceId $id -Command 'emu avd name' -Verbose:$VerbosePreference | Select-Object -First 1)
-                continue
+            $cache = Get-CacheValue -DeviceId $id -ErrorAction SilentlyContinue
+            if ($null -ne $cache) {
+                Write-Verbose "Get cached device name: '$cache'"
+                [string] $cache
             }
-            Get-AdbProperty -DeviceId $id -Name 'ro.product.model' -Verbose:$VerbosePreference
+            else {
+                $result = if ((Test-AdbEmulator -DeviceId $id)) {
+                    [string] (Invoke-AdbExpression -DeviceId $id -Command 'emu avd name' -Verbose:$VerbosePreference | Select-Object -First 1)
+                }
+                else {
+                    Get-AdbProperty -DeviceId $id -Name 'ro.product.model' -Verbose:$VerbosePreference
+                }
+
+                Set-CacheValue -DeviceId $id -Value $result
+
+                [string] $result
+            }
         }
     }
 }
