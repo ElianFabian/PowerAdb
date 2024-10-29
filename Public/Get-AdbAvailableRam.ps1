8992c74f0915f8073429b31ed070784cd4be996a
Elián Fabián <elian.fabian.2001@gmail.com>
2024-10-29 23:16:51 +0100

    Add ram releated functions

---
+++ Public/Get-AdbAvailableRam.ps1
@@ -0,0 +1,19 @@
+function Get-AdbAvailableRam {
+
+    [CmdletBinding()]
+    [OutputType([uint64])]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]] $DeviceId
+    )
+
+    process {
+        foreach ($id in $DeviceId) {
+            Invoke-AdbExpression -DeviceId $id -Command "shell cat /proc/meminfo" -Verbose:$VerbosePreference `
+            | Select-String -Pattern "MemAvailable:\s*(\d+)\skB" -AllMatches `
+            | Select-Object -First 1 `
+            | ForEach-Object { $_.Matches.Groups[1].Value } `
+            | ForEach-Object { ([uint64] $_) * 1024 }
+        }
+    }
+}
---
+++ Public/Get-AdbTotalRam.ps1
@@ -0,0 +1,28 @@
+function Get-AdbTotalRam {
+
+    [CmdletBinding()]
+    [OutputType([uint64])]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]] $DeviceId
+    )
+
+    process {
+        foreach ($id in $DeviceId) {
+            $cachedTotalRam = Get-CacheValue -DeviceId $id -ErrorAction SilentlyContinue
+            if ($null -ne $cachedTotalRam) {
+                Write-Verbose "Get cached total RAM for device with id '$id'"
+                [uint64] $cachedTotalRam
+            }
+            else {
+                $result = Invoke-AdbExpression -DeviceId $id -Command "shell cat /proc/meminfo" -Verbose:$VerbosePreference `
+                | Select-String -Pattern "MemTotal:\s*(\d+)\skB" -AllMatches `
+                | Select-Object -First 1 `
+                | ForEach-Object { $_.Matches.Groups[1].Value } `
+                | ForEach-Object { ([uint64] $_) * 1024 }
+                Set-CacheValue -DeviceId $id -Value $result
+                [uint64] $result
+            }
+        }
+    }
+}
