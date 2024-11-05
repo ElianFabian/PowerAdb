373e203aee1aea191eca2f19e40457505528a592
Elián Fabián <elian.fabian.2001@gmail.com>
2024-11-05 17:12:22 +0100

    Improve Get-AdbAppInfo support for older API levels

--- Public/Get-AdbAppInfo.ps1
+++ Public/Get-AdbAppInfo.ps1
@@ -100,17 +100,19 @@ function Get-AdbAppInfo {
                     | Select-Object -ExpandProperty Matches -First 1 `
                     | ForEach-Object { $_.Value }
 
-                    $packageFlagsValue = [PSCustomObject]@{
-                        AllowBackup          = $rawFlags.Contains("ALLOW_BACKUP")
-                        AllowClearUserData   = $rawFlags.Contains("ALLOW_CLEAR_USER_DATA")
-                        AllowTaskReparenting = $rawFlags.Contains("ALLOW_TASK_REPARENTING")
-                        Debuggable           = $rawFlags.Contains("DEBUGGABLE")
-                        DirectBootAware      = $rawFlags.Contains("PARTIALLY_DIRECT_BOOT_AWARE")
-                        HasCode              = $rawFlags.Contains("HAS_CODE")
-                        LargeHeap            = $rawFlags.Contains("LARGE_HEAP")
-                        KillAfterRestore     = $rawFlags.Contains("KILL_AFTER_RESTORE")
-                        TestOnly             = $rawFlags.Contains("TEST_ONLY")
-                        VmSafeMode           = $rawFlags.Contains("VM_SAFE_MODE")
+                    if ($rawFlags) {
+                        $packageFlagsValue = [PSCustomObject]@{
+                            AllowBackup          = $rawFlags.Contains("ALLOW_BACKUP")
+                            AllowClearUserData   = $rawFlags.Contains("ALLOW_CLEAR_USER_DATA")
+                            AllowTaskReparenting = $rawFlags.Contains("ALLOW_TASK_REPARENTING")
+                            Debuggable           = $rawFlags.Contains("DEBUGGABLE")
+                            DirectBootAware      = $rawFlags.Contains("PARTIALLY_DIRECT_BOOT_AWARE")
+                            HasCode              = $rawFlags.Contains("HAS_CODE")
+                            LargeHeap            = $rawFlags.Contains("LARGE_HEAP")
+                            KillAfterRestore     = $rawFlags.Contains("KILL_AFTER_RESTORE")
+                            TestOnly             = $rawFlags.Contains("TEST_ONLY")
+                            VmSafeMode           = $rawFlags.Contains("VM_SAFE_MODE")
+                        }
                     }
                 }
                 if ($AllInfo -or $PrivatePackageFlags) {
@@ -119,11 +121,13 @@ function Get-AdbAppInfo {
                     | Select-Object -ExpandProperty Matches -First 1 `
                     | ForEach-Object { $_.Value }
 
-                    $privatePackageFlagsValue = [PSCustomObject]@{
-                        AllowAudioPlaybackCapture     = $privateRawFlags.Contains("ALLOW_AUDIO_PLAYBACK_CAPTURE")
-                        AllowNativeHeapPointerTagging = $privateRawFlags.Contains("PRIVATE_FLAG_ALLOW_NATIVE_HEAP_POINTER_TAGGING")
-                        # The opposite flag for ResizableActivity is "PRIVATE_FLAG_ACTIVITIES_RESIZE_MODE_UNRESIZEABLE"
-                        ResizableActivity             = $privateRawFlags.Contains("PRIVATE_FLAG_ACTIVITIES_RESIZE_MODE_RESIZEABLE")
+                    if ($privateRawFlags) {
+                        $privatePackageFlagsValue = [PSCustomObject]@{
+                            AllowAudioPlaybackCapture     = $privateRawFlags.Contains("ALLOW_AUDIO_PLAYBACK_CAPTURE")
+                            AllowNativeHeapPointerTagging = $privateRawFlags.Contains("PRIVATE_FLAG_ALLOW_NATIVE_HEAP_POINTER_TAGGING")
+                            # The opposite flag for ResizableActivity is "PRIVATE_FLAG_ACTIVITIES_RESIZE_MODE_UNRESIZEABLE"
+                            ResizableActivity             = $privateRawFlags.Contains("PRIVATE_FLAG_ACTIVITIES_RESIZE_MODE_RESIZEABLE")
+                        }
                     }
                 }
 
