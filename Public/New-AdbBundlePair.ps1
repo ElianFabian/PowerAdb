b5df60f63b96ee85d4e1492790849ca2d9f6d21e
Elián Fabián <elian.fabian.2001@gmail.com>
2024-11-08 20:29:31 +0100

    Add New-AdbIntent and New-AdbBundlePair
    In future these functions will be used for some functions releated to 'adb shell am'.

---
+++ Public/New-AdbBundlePair.ps1
@@ -0,0 +1,139 @@
+function New-AdbBundlePair {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string] $Key,
+
+        [Parameter(Mandatory, ParameterSetName = 'String')]
+        [string] $String,
+
+        [Parameter(Mandatory, ParameterSetName = 'Boolean')]
+        [bool] $Boolean,
+
+        [Parameter(Mandatory, ParameterSetName = 'Int')]
+        [int] $Int,
+
+        [Parameter(Mandatory, ParameterSetName = 'Long')]
+        [long] $Long,
+
+        [Parameter(Mandatory, ParameterSetName = 'Float')]
+        [float] $Float,
+
+        [Parameter(Mandatory, ParameterSetName = 'Uri')]
+        [uri] $Uri,
+
+        [Parameter(Mandatory, ParameterSetName = 'ComponentName')]
+        [string] $ApplicationId,
+
+        [Parameter(Mandatory, ParameterSetName = 'ComponentName')]
+        [string] $ClassName,
+
+        [Parameter(Mandatory, ParameterSetName = 'IntArray')]
+        [int[]] $IntArray,
+
+        [Parameter(Mandatory, ParameterSetName = 'IntArrayList')]
+        [int[]] $IntArrayList,
+
+        [Parameter(Mandatory, ParameterSetName = 'LongArray')]
+        [long[]] $LongArray,
+
+        [Parameter(Mandatory, ParameterSetName = 'LongArrayList')]
+        [long[]] $LongArrayList,
+
+        [Parameter(Mandatory, ParameterSetName = 'FloatArray')]
+        [float[]] $FloatArray,
+
+        [Parameter(Mandatory, ParameterSetName = 'FloatArrayList')]
+        [float[]] $FloatArrayList,
+
+        [Parameter(Mandatory, ParameterSetName = 'StringArray')]
+        [string[]] $StringArray,
+
+        [Parameter(Mandatory, ParameterSetName = 'StringArrayList')]
+        [string[]] $StringArrayList
+    )
+
+    $pair = [PSCustomObject]@{
+        Key  = $Key
+        Type = $PSCmdlet.ParameterSetName
+    }
+
+    switch ($PSCmdlet.ParameterSetName) {
+        'String' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $String
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--es '$($this.Key)' ""'$($this.Value)'""" }
+        }
+        'Boolean' {
+            $booleanStr = if ($Boolean) { 'true' } else { 'false' }
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $booleanStr
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--ez '$($this.Key)' $($this.Value)" }
+        }
+        'Int' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $Int
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--ei '$($this.Key)' $($this.Value)" }
+        }
+        'Long' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $Long
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--el '$($this.Key)' $($this.Value)" }
+        }
+        'Float' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $Float
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value {
+                "--ef '$($this.Key)' $($this.Value.ToString([System.Globalization.CultureInfo]::InvariantCulture))"
+            }
+        }
+        'Uri' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $Uri
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--eu '$($this.Key)' '$($this.Value)'" }
+        }
+        'ComponentName' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value "$ApplicationId/$ClassName"
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--ecn '$($this.Key)' '$($this.Value)'" }
+        }
+        'IntArray' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $IntArray
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--eia '$($this.Key)' $($this.Value -join ',')" }
+        }
+        'IntArrayList' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $IntArrayList
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--eial '$($this.Key)' $($this.Value -join ',')" }
+        }
+        'LongArray' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $LongArray
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--ela '$($this.Key)' $($this.Value -join ',')" }
+        }
+        'LongArrayList' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $LongArrayList
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value { "--elal '$($this.Key)' $($this.Value -join ',')" }
+        }
+        'FloatArray' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $FloatArray
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value {
+                "--efa '$($this.Key)' $(($this.Value | ForEach-Object { $_.ToString([System.Globalization.CultureInfo]::InvariantCulture) }) -join ',')"
+            }
+        }
+        'FloatArrayList' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $FloatArrayList
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value {
+                "--efal '$($this.Key)' $(($this.Value | ForEach-Object { $_.ToString([System.Globalization.CultureInfo]::InvariantCulture) }) -join ',')"
+            }
+        }
+        'StringArray' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $StringArray
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value {
+                $stringArrayStr = ($this.Value | ForEach-Object { "'$_'" }) -join ','
+                "--esa '$($this.Key)' ""$stringArrayStr""" 
+            }
+        }
+        'StringArrayList' {
+            $pair | Add-Member -MemberType NoteProperty -Name Value -Value $StringArrayList
+            $pair | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value {
+                $stringArrayListStr = ($this.Value | ForEach-Object { "'$_'" }) -join ','
+                "--esal '$($this.Key)' ""$stringArrayListStr""" 
+            }
+        }
+    }
+
+    return $pair
+}
---
+++ Public/New-AdbIntent.ps1
@@ -0,0 +1,85 @@
+# https://developer.android.com/tools/adb#IntentSpec
+function New-AdbIntent {
+
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
+    param (
+        [string] $Action = $null,
+
+        [uri] $Data = $null,
+
+        [string] $Type = $null,
+
+        [string] $Identifier = $null,
+
+        [string[]] $Category = $null,
+
+        [Parameter(Mandatory, ParameterSetName = 'ComponentName')]
+        [string] $ApplicationId = $null,
+
+        [Parameter(Mandatory, ParameterSetName = 'ComponentName')]
+        [string] $ComponentClassName = $null,
+        
+        [int] $Flags = 0,
+
+        [switch] $Selector,
+
+        [scriptblock] $Extras = $null
+    )
+
+    if ($Selector.IsPresent -and (-not $Data -or -not $Type)) {
+        throw "The -Selector parameter can only be used when both -Data and -Type are set."
+    }
+
+    $intent = [PSCustomObject] @{
+        Action        = $Action
+        Data          = $Data
+        Type          = $Type
+        Identifier    = $Identifier
+        Category      = $Category
+        Flags         = $Flags
+        Selector      = $Selector
+    }
+
+    if ($Extras) {
+        $intent | Add-Member -MemberType NoteProperty -Name Extras -Value ($Extras.Invoke())
+    }
+    if ($PSCmdlet.ParameterSetName -eq 'ComponentName') {
+        $componentName = "$ApplicationId/$ComponentClassName"
+        $intent | Add-Member -MemberType NoteProperty -Name ComponentName -Value $componentName
+    }
+
+    $intent | Add-Member -MemberType ScriptMethod -Name ToAdbArguments -Value {
+        $adbArguments = ""
+        if ($this.Action) {
+            $adbArguments += " -a '$($this.Action)'"
+        }
+        if ($this.Data) {
+            $adbArguments += " -d '$($this.Data)'"
+        }
+        if ($this.Type) {
+            $adbArguments += " -t '$($this.Type)'"
+        }
+        if ($this.Identifier) {
+            $adbArguments += " -i '$($this.Identifier)'"
+        }
+        if ($this.Category) {
+            $adbArguments += " -c $(($this.Category -join ",") | ForEach-Object { "'$_'" })"
+        }
+        if ($this.ComponentName) {
+            $adbArguments += " -n '$($this.ComponentName)'"
+        }
+        if ($this.Flags) {
+            $adbArguments += " -f 0x$($this.Flags.ToString('x8'))"
+        }
+        $this.Extras | ForEach-Object {
+            $adbArguments += " $($_.ToAdbArguments())"
+        }
+        if ($this.Selector) {
+            $adbArguments += " --selector"
+        }
+
+        $adbArguments.Trim()
+    }
+
+    return $intent
+}
