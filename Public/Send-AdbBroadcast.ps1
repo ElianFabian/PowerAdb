1679d17c5d4f7840cf78bc0ffdb7bbf1763261b7
Elián Fabián <elian.fabian.2001@gmail.com>
2024-11-10 11:58:01 +0100

    Add Send-AdbBroadcast

---
+++ Public/Send-AdbBroadcast.ps1
@@ -0,0 +1,21 @@
+function Send-AdbBroadcast {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]] $DeviceId,
+
+        [Parameter(Mandatory)]
+        [PSCustomObject] $Intent
+    )
+
+    begin {
+        $intentArgs = $Intent.ToAdbArguments()
+    }
+
+    process {
+        foreach ($id in $DeviceId) {
+            Invoke-AdbExpression -DeviceId $id -Command "shell am broadcast $intentArgs"
+        }
+    }
+}
