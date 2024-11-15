5ce2d217a38caa5f66d4ea11d369fc7c835c82d8
Elián Fabián <elian.fabian.2001@gmail.com>
2024-11-15 20:09:26 +0100

    Change api level limit and error message in Send-AdbKeyCombination

--- Public/Send-AdbKeyCombination.ps1
+++ Public/Send-AdbKeyCombination.ps1
@@ -14,11 +14,7 @@ function Send-AdbKeyCombination {
         foreach ($id in $DeviceId) {
             $apiLevel = [uint32] (Get-AdbProperty -DeviceId $id -Name ro.build.version.sdk -Verbose:$false)
             if ($apiLevel -le 30) {
-                Write-Error "'adb shell input keycombination' is not available for api levels lower or equal to 30. Device id: '$id', api level: '$apiLevel'"
-                continue
-            }
-            if ($apiLevel -le 33) {
-                Write-Error "'adb shell input keycombination' is available for api levels [31, 32, 33] (the command exists but it doesn't seem to work properly). Device id: '$id', api level: '$apiLevel'"
+                Write-Error "Send key combination is not supported for device with id '$id' with API level of '$apiLevel'. Only API levels 30 and above are supported."
                 continue
             }
             Invoke-AdbExpression -DeviceId $id -Command "shell input keycombination $($KeyCodes | ForEach-Object { "KEYCODE_$_" })" -Verbose:$VerbosePreference | Out-Null
