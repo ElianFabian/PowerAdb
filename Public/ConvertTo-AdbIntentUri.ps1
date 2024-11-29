026c4c91da2304c2aa6ba0be4c93658e0183dfae
Elián Fabián <elian.fabian.2001@gmail.com>
2024-11-29 18:13:41 +0100

    Fix ConvertToAdbUri and ConvertToAdbIntentUri
    - Add DeviceId parameter to both functions.

--- Public/ConvertTo-AdbIntentUri.ps1
+++ Public/ConvertTo-AdbIntentUri.ps1
@@ -3,11 +3,20 @@ function ConvertTo-AdbIntentUri {
     [OutputType([string[]])]
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]] $DeviceId,
+
         [Parameter(Mandatory)]
         [PSCustomObject] $Intent
     )
 
-    $intentArgs = $Intent.ToAdbArguments()
+    begin {
+        $intentArgs = $element.ToAdbArguments()
+    }
 
-    Invoke-AdbExpression -Command "shell am to-intent-uri $intentArgs" -Verbose:$VerbosePreference -WhatIf:$false -Confirm:$false
+    process {
+        foreach ($id in $DeviceId) {
+            Invoke-AdbExpression -DeviceId $id -Command "shell am to-intent-uri $intentArgs" -Verbose:$VerbosePreference -WhatIf:$false -Confirm:$false
+        }
+    }
 }
