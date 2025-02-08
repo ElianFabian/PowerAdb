3d5cbf21c7356c218139eb7b5a62e0031b85cc62
Elián Fabián <elian.fabian.2001@gmail.com>
2025-02-09 00:06:54 +0100

    Add Get-AdbLogcat

---
+++ Public/Get-AdbLogcat.ps1
@@ -0,0 +1,197 @@
+function Get-AdbLogcat {
+
+    [CmdletBinding(DefaultParameterSetName = "Default")]
+    param (
+        [Parameter(Mandatory)]
+        [string] $DeviceId,
+
+        [ValidateSet(
+            "Main",
+            "System",
+            "Radio",
+            "Events",
+            "Crash",
+            "Kernel",
+            "Security"
+        )]
+        [string] $Buffer,
+
+        [switch] $LogsBeforeLastReboot,
+
+        [switch] $ExitAfterDump,
+
+        [string] $ParentProcessId,
+
+        [ValidateSet(
+            "Brief",
+            "Long",
+            "Process",
+            "Raw",
+            "Tag",
+            "Thread",
+            "ThreadTime",
+            "Time"
+        )]
+        [string] $Format,
+
+        [switch] $Dividers,
+
+        [switch] $Binary,
+
+        [Parameter(ParameterSetName = "Default")]
+        [Parameter(ParameterSetName = "Last")]
+        [Parameter(ParameterSetName = "LastAt")]
+        [Parameter(ParameterSetName = "MaxCount")]
+        [Parameter(Mandatory, ParameterSetName = "Print")]
+        [string] $Pattern,
+
+        [Parameter(ParameterSetName = "Last")]
+        [int] $Last,
+
+        # Valid formats:
+        # - MM-DD hh:mm:ss.mmm...
+        # - YYYY-MM-DD hh:mm:ss.mmm...
+        # - sssss.mmm...
+        [Parameter(ParameterSetName = "LastAt")]
+        [ValidatePattern(
+            "^\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+$|^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+$|^\d+\.\d+$"
+        )]
+        [string] $LastAt,
+
+        [Parameter(ParameterSetName = "Last")]
+        [Parameter(ParameterSetName = "LastAt")]
+        [switch] $Block,
+
+        [Parameter(Mandatory, ParameterSetName = "Print")]
+        [Parameter(ParameterSetName = "MaxCount")]
+        [int] $MaxCount,
+
+        [Parameter(ParameterSetName = "Print")]
+        [switch] $PrintUntil,
+
+        [string] $Tag,
+
+        [ValidateSet(
+            "Verbose",
+            "Debug",
+            "Info",
+            "Warn",
+            "Error",
+            "Fatal",
+            "Silent"
+        )]
+        [string] $Priority
+    )
+
+    begin {
+        $bufferLowerCase = switch ($Buffer) {
+            "Main" { "main" }
+            "System" { "system" }
+            "Radio" { "radio" }
+            "Events" { "events" }
+            "Crash" { "crash" }
+            "Kernel" { "kernel" }
+            "Security" { "security" }
+            default { $null }
+        }
+        $priorityCapital = switch ($Priority) {
+            "Verbose" { "V" }
+            "Debug" { "D" }
+            "Info" { "I" }
+            "Warn" { "W" }
+            "Error" { "E" }
+            "Fatal" { "F" }
+            "Silent" { "S" }
+            default { $null }
+        }
+        $formatLowerCase = switch ($Format) {
+            "Brief" { "brief" }
+            "Long" { "long" }
+            "Process" { "process" }
+            "Raw" { "raw" }
+            "Tag" { "tag" }
+            "Thread" { "thread" }
+            "ThreadTime" { "threadtime" }
+            "Time" { "time" }
+            default { $null }
+        }
+
+        if ($bufferLowerCase) {
+            $bufferArg = " --buffer=$bufferLowerCase"
+        }
+        if ($LogsBeforeLastReboot) {
+            $lastArg = " --last"
+        }
+        if ($ExitAfterDump) {
+            $dArg = " -d"
+        }
+        if ($ParentProcessId) {
+            $pidArg = " --pid=$ParentProcessId"
+        }
+        if ($Format) {
+            $formatArg = " --format=$formatLowerCase"
+        }
+        if ($Dividers) {
+            $dividersArg = " --dividers"
+        }
+        if ($Binary) {
+            $binaryArg = " --binary"
+        }
+        if ($Pattern) {
+            $regexArg = " --regex=$Pattern"
+        }
+        if ($MaxCount) {
+            $countOrTimeFilterArg = " --max-count=$MaxCount"
+        }
+        if ($PrintUntil) {
+            $printArg = " --print"
+        }
+
+        if ($Block) {
+            if ($Last) {
+                $countOrTimeFilterArg = " -T $Last"
+            }
+            elseif ($LastAt) {
+                $countOrTimeFilterArg = " -T '$LastAt'"
+            }
+        }
+        else {
+            if ($Last) {
+                $countOrTimeFilterArg = " -t $Last"
+            }
+            elseif ($LastAt) {
+                $countOrTimeFilterArg = " -t '$LastAt'"
+            }
+        }
+
+        if ($Tag -and $priorityCapital) {
+            $filterspec = " $Tag`:$priorityCapital"
+        }
+        elseif ($Tag) {
+            $filterspec = " $Tag"
+        }
+        elseif ($priorityCapital) {
+            $filterspec = " *:$priorityCapital"
+        }
+
+        $adbArgs = @(
+            $bufferArg,
+            $lastArg,
+            $dArg,
+            $pidArg,
+            $formatArg,
+            $dividersArg,
+            $binaryArg,
+            $regexArg,
+            $printArg,
+            $countOrTimeFilterArg,
+            $filterspec
+        ) -join ""
+    }
+
+    process {
+        foreach ($id in $DeviceId) {
+            Invoke-AdbExpression -DeviceId $DeviceId -Command "logcat$adbArgs"
+        }
+    }
+}
