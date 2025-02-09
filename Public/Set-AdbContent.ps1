4b9830e9c1c472d8b610fcc3ef7d6cb17fbbe4c4
Elián Fabián <elian.fabian.2001@gmail.com>
2025-02-10 00:41:34 +0100

    Revert "Fix process of -Path parameter"
    This reverts commit f62cc16c1c4a0b22c1a7affa428064055b520b65.
    
    I don't remember what was the problem with just using single quotes, but not this doesn't work in here for example:
    
    adb pull '"/data/app/~~jIAjtqp0YinqFEmODwmHIw==/com.some.app-qwf3SP5lqPcoQpMFsyiEEg==/base.apk"' '"base.apk"'
    adb: error: failed to stat remote object '"/data/app/~~jIAjtqp0YinqFEmODwmHIw==/com.some.app-qwf3SP5lqPcoQpMFsyiEEg==/base.apk"': No such file or directory
    
    We'll revert this for now and see if we found any issue

--- Public/Set-AdbContent.ps1
+++ Public/Set-AdbContent.ps1
@@ -24,7 +24,7 @@ function Set-AdbContent {
 
     process {
         foreach ($id in $DeviceId) {
-            Invoke-AdbExpression -DeviceId $id -Command "shell$runAsCommand ""echo '""$Content""' > '$RemotePath'""" -Verbose:$VerbosePreference
+            Invoke-AdbExpression -DeviceId $id -Command "shell$runAsCommand ""echo '$Content' > '$RemotePath'""" -Verbose:$VerbosePreference
         }
     }
 }
