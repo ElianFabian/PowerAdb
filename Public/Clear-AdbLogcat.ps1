d103897e8e8052cbf4ff127da8d6708ab14f19f3
Elián Fabián <elian.fabian.2001@gmail.com>
2025-02-09 20:45:06 +0100

    Add Force parameter to Clear-AdbLogcat

--- Public/Clear-AdbLogcat.ps1
+++ Public/Clear-AdbLogcat.ps1
@@ -16,7 +16,9 @@ function Clear-AdbLogcat {
         )]
         [string] $Buffer,
 
-        [string] $Pattern
+        [string] $Pattern,
+
+        [switch] $Force
     )
 
     begin {
@@ -45,7 +47,13 @@ function Clear-AdbLogcat {
 
     process {
         foreach ($id in $DeviceId) {
-            Invoke-AdbExpression -DeviceId $id -Command "logcat --clear$adbArgs" -Verbose:$VerbosePreference
+            do {
+                $logcatError = Invoke-AdbExpression -DeviceId $id -Command "logcat -c$adbArgs" -Verbose:$VerbosePreference -WhatIf:$false -Confirm:$false 2>&1
+                if ((-not $Force -or $logcatError -notmatch "logcat: failed to clear the '\w+' log.") -and $logcatError -and $logcatError -is [System.Management.Automation.ErrorRecord]) {
+                    Write-Error -Exception $logcatError.Exception
+                }
+            }
+            while ($Force -and $logcatError -and $logcatError -is [System.Management.Automation.ErrorRecord] -and $logcatError.Exception.Message -match "logcat: failed to clear the '\w+' log.")
         }
     }
 }
