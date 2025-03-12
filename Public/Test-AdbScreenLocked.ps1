00f58bd6edb0be03db903d38709eb6470484f7f8
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-12 01:31:22 +0100

    Add Test-AdbScreenLocked

---
+++ Public/Test-AdbScreenLocked.ps1
@@ -0,0 +1,24 @@
+function Test-AdbScreenLocked {
+
+    [OutputType([bool[]])]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]] $DeviceId
+    )
+
+    foreach ($id in $DeviceId) {
+        $apiLevel = Get-AdbApiLevel -DeviceId $id -Verbose:$false
+        if ($apiLevel -lt 23) {
+            Write-ApiLevelError -DeviceId $id -ApiLevelLessThan 23
+            continue
+        }
+
+        # https://android.stackexchange.com/a/245699
+        $rawData = Invoke-AdbExpression -DeviceId $id -Command "shell dumpsys deviceidle" -Verbose:$VerbosePreference
+        
+        $rawData | Select-String -Pattern 'mScreenLocked=(true|false)' | ForEach-Object {
+            $_.Matches.Groups[1].Value -eq 'true'
+        }
+    }
+}
