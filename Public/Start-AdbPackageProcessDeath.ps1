7b8977f885a8d698e5664769bcc22be3626092ee
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-19 16:52:24 +0100

    Add -Force parameter to Start-AdbPackageProcessDeath

--- Public/Start-AdbPackageProcessDeath.ps1
+++ Public/Start-AdbPackageProcessDeath.ps1
@@ -7,12 +7,20 @@ function Start-AdbPackageProcessDeath {
         [string[]] $DeviceId,
 
         [Parameter(Mandatory)]
-        [string[]] $PackageName
+        [string[]] $PackageName,
+
+        [switch] $Force
     )
 
     process {
         foreach ($id in $DeviceId) {
             foreach ($package in $PackageName) {
+                if ($Force) {
+                    $topActivity = Get-AdbTopActivity -DeviceId $id -Verbose:$false
+                    if ($topActivity.PackageName -eq $package) {
+                        Send-AdbKeyEvent -DeviceId $id -KeyCode HOME -Verbose:$VerbosePreference
+                    }
+                }
                 Invoke-AdbExpression -DeviceId $id -Command "shell am kill '$package'" -Verbose:$VerbosePreference
             }
         }
