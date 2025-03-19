d678c02caead1fdfee967ae0a0ecd06f4ed364ae
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-19 16:53:28 +0100

    Change return type in Get-AdbTopActivity
    It now returns a PSCustomObject.
    This also fixes  the  issue that it returned the package name mixed with the activity name

--- Public/Get-AdbTopActivity.ps1
+++ Public/Get-AdbTopActivity.ps1
@@ -8,13 +8,21 @@ function Get-AdbTopActivity {
     )
 
     process {
-        $DeviceId | Invoke-AdbExpression -Command "shell dumpsys activity activities" -Verbose:$VerbosePreference -WhatIf:$false -Confirm:$false `
-        | Select-String -Pattern "(topResumedActivity=.+|mResumedActivity: ActivityRecord){.+ .+ (.+) .+}" -AllMatches `
-        | Select-Object -ExpandProperty Matches -First 1 `
-        | Select-Object -ExpandProperty Groups -Last 1 `
-        | Select-Object -ExpandProperty Value -Last 1 `
-        | ForEach-Object {
-            $_.Replace('/', '').Trim("}")
+        foreach ($id in $DeviceId) {
+            Invoke-AdbExpression -DeviceId $id -Command "shell dumpsys activity activities" -Verbose:$VerbosePreference -WhatIf:$false -Confirm:$false `
+            | Select-String -Pattern "(topResumedActivity=.+|mResumedActivity: ActivityRecord){.+ .+ (.+) .+}" -AllMatches `
+            | Select-Object -ExpandProperty Matches -First 1 `
+            | Select-Object -ExpandProperty Groups -Last 1 `
+            | Select-Object -ExpandProperty Value -Last 1 `
+            | ForEach-Object {
+                $packageName, $activityClassName = $_.Split('/')
+
+                [PSCustomObject]@{
+                    DeviceId = $id
+                    PackageName = $packageName
+                    ActivityClassName = $activityClassName
+                }
+            }
         }
     }
 }
