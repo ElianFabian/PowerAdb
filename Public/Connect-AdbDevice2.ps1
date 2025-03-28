b0479a68dffd09a905d912a44cfbf1e926f43ae4
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-28 17:43:11 +0100

    Add more checks in Connect-AdbDevice2
    This is useful to know why you can't connect your device

--- Public/Connect-AdbDevice2.ps1
+++ Public/Connect-AdbDevice2.ps1
@@ -13,6 +13,14 @@ function Connect-AdbDevice2 {
                 Write-Error "Wireless debugging is not enabled on device $id."
                 return
             }
+
+            $currentPcWifiName = Get-NetConnectionProfile | Select-Object -ExpandProperty Name
+            $deviceWifiName = Get-AdbNetConnectionProfileName -DeviceId $id -Verbose:$false
+            if ($deviceWifiName -cne $currentPcWifiName) {
+                Write-Error "Device '$id' is connected to a different Wi-Fi network. PC: '$currentPcWifiName', Device: '$deviceWifiName'"
+                return
+            }
+
             Start-AdbTcpIp -DeviceId $id -Port 5555 -Verbose:$VerbosePreference
             $ipAdress = Get-AdbLocalNetworkIp -DeviceId $id -Wait -Verbose:$false
             Connect-AdbDevice -IpAddress $ipAdress -Port 5555 -Verbose:$VerbosePreference
