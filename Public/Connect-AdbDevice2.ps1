6b6f665fb986770a98b3c213309824513a12c9e5
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-28 17:20:18 +0100

    Add check for wireless debugging enabled in Connect-AdbDevice2

--- Public/Connect-AdbDevice2.ps1
+++ Public/Connect-AdbDevice2.ps1
@@ -8,6 +8,11 @@ function Connect-AdbDevice2 {
 
     process {
         foreach ($id in $DeviceId) {
+            $isWirelessDebuggingEnabled = Get-AdbSetting -DeviceId $id -Namespace Global -Key adb_wifi_enabled -Verbose:$false
+            if ($isWirelessDebuggingEnabled -eq 0) {
+                Write-Error "Wireless debugging is not enabled on device $id."
+                return
+            }
             Start-AdbTcpIp -DeviceId $id -Port 5555 -Verbose:$VerbosePreference
             $ipAdress = Get-AdbLocalNetworkIp -DeviceId $id -Wait -Verbose:$false
             Connect-AdbDevice -IpAddress $ipAdress -Port 5555 -Verbose:$VerbosePreference
