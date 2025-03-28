85030a858701633fbec31d9cf4d73de263cfabce
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-28 17:42:26 +0100

    Add Get-AdbNetConnectionProfileName

---
+++ Public/Get-AdbNetConnectionProfileName.ps1
@@ -0,0 +1,18 @@
+function Get-AdbNetConnectionProfileName {
+
+    [OutputType([string[]])]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string] $DeviceId
+    )
+
+    process {
+        foreach ($id in $DeviceId) {
+            Invoke-AdbExpression -DeviceId $id -Command 'shell dumpsys netstats' `
+            | Select-String -Pattern 'iface=wlan.*networkId="(.+)"'
+            | Select-Object -First 1 `
+            | ForEach-Object { $_.Matches[0].Groups[1].Value }
+        }
+    }
+}
