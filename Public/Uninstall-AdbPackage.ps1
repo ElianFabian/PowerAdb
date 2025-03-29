9ff9381da73b904c52523029e49d95fb1ca1a0ee
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-29 18:57:15 +0100

    Add -UserId and -VersionCode parameters Uninstall-AdbPackagein

--- Public/Uninstall-AdbPackage.ps1
+++ Public/Uninstall-AdbPackage.ps1
@@ -6,13 +6,33 @@ function Uninstall-AdbPackage {
         [string[]] $DeviceId,
 
         [Parameter(Mandatory)]
-        [string[]] $PackageName
+        [string[]] $PackageName,
+
+        [switch] $KeepDataAndCache,
+
+        [AllowNull()]
+        [Nullable[uint32]] $UserId,
+
+        [AllowNull()]
+        [Nullable[uint32]] $VersionCode
     )
+
+    begin {
+        if ($KeepDataAndCache) {
+            $keepArg = " -k"
+        }
+        if ($null -ne $UserId) {
+            $userArg = " --user $UserId"
+        }
+        if ($null -ne $VersionCode) {
+            $versionCodeArg = " --versionCode $VersionCode"
+        }
+    }
 
     process {
         foreach ($id in $DeviceId) {
             foreach ($package in $PackageName) {
-                Invoke-AdbExpression -DeviceId $id -Command "uninstall '$package'" -Verbose:$VerbosePreference
+                Invoke-AdbExpression -DeviceId $id -Command "uninstall '$package'$keepArg$userArg$versionCodeArg" -Verbose:$VerbosePreference
             }
         }
     }
