c7001930ee8f643864c1357e5d7769f0ae856520
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-29 11:57:20 +0100

    Improve Send-AdbText encoding

--- Public/Send-AdbText.ps1
+++ Public/Send-AdbText.ps1
@@ -27,31 +27,14 @@ function Send-AdbText {
         }
 
         foreach ($id in $DeviceId) {
-            $sb = [System.Text.StringBuilder]::new($Text)
+            $sanitizedText = ConvertTo-ValidAdbStringArgument $Text
 
-            $sb.Replace('(', '\(') > $null
-            $sb.Replace(')', '\)') > $null
-            $sb.Replace('<', '\<') > $null
-            $sb.Replace('>', '\>') > $null
-            $sb.Replace('|', '\|') > $null
-            $sb.Replace(';', '\;') > $null
-            $sb.Replace('&', '\&') > $null
-            $sb.Replace('\', '\\') > $null
-            $sb.Replace('~', '\~') > $null
-            $sb.Replace("'", "\'") > $null
-            $sb.Replace('`', '\`') > $null
-            $sb.Replace('%', '\%') > $null
-            $sb.Replace(" ", "%s") > $null
-            $sb.Replace('"', '\"') > $null
-
-            $encodedText = $sb.ToString()
-
-            Invoke-AdbExpression -DeviceId $id -Command "shell input text '$encodedText'" -Verbose:$VerbosePreference
+            Invoke-AdbExpression -DeviceId $id -Command "shell input text $sanitizedText" -Verbose:$VerbosePreference
         }
     }
 }
 
 
-$script:Ascii = [System.Text.Encoding]::ASCII
 
+$script:Ascii = [System.Text.Encoding]::ASCII
 $script:Latin1 = [System.Text.Encoding]::GetEncoding("ISO-8859-1")
