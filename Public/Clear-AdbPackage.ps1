675b2a37794feae7939f67b76c0939f0be100ac3
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-30 14:19:23 +0200

    Add -UserId parameter to Clear-AdbPackage

--- Public/Clear-AdbPackage.ps1
+++ Public/Clear-AdbPackage.ps1
@@ -5,14 +5,23 @@ function Clear-AdbPackage {
         [Parameter(Mandatory, ValueFromPipeline)]
         [string[]] $DeviceId,
 
+        [AllowNull()]
+        [Nullable[uint32]] $UserId,
+
         [Parameter(Mandatory)]
         [string[]] $PackageName
     )
 
+    begin {
+        if ($null -ne $UserId) {
+            $userArg = " --user $UserId"
+        }
+    }
+
     process {
         foreach ($id in $DeviceId) {
             foreach ($package in $PackageName) {
-                $result = Invoke-AdbExpression -DeviceId $id -Command "shell pm clear $package" -Verbose:$VerbosePreference
+                $result = Invoke-AdbExpression -DeviceId $id -Command "shell pm clear$userArg $package" -Verbose:$VerbosePreference
                 Write-Verbose "Clear data in device with id '$id' from '$package': $result"
             }
         }
