20586f0f14249fb32eaa033f9d5d860ba0494734
Elián Fabián <elian.fabian.2001@gmail.com>
2025-03-30 14:22:05 +0200

    Add Find-AdbBroadcastReceiver

---
+++ Public/Find-AdbBroadcastReceiver.ps1
@@ -0,0 +1,33 @@
+function Find-AdbBroadcastReceiver {
+
+    [OutputType([string[]])]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string] $DeviceId,
+
+        [AllowNull()]
+        [Nullable[uint32]] $UserId,
+
+        [Parameter(Mandatory)]
+        [PSCustomObject] $Intent
+    )
+
+    begin {
+        if ($null -ne $UserId) {
+            $userArg = " --user $UserId"
+        }
+        $intentArgs = $Intent.ToAdbArguments()
+    }
+
+    process {
+        foreach ($id in $DeviceId) {
+            Invoke-AdbExpression -DeviceId $id -Command "shell pm query-services --brief --components $userArg$intentArgs" -Verbose:$VerbosePreference
+        }
+    }
+}
+
+# TODO: Add support for --query-flags
+# query-receivers [--brief] [--components] [--query-flags FLAGS]
+#      [--user USER_ID] INTENT
+#   Prints all broadcast receivers that can handle the given INTENT.
