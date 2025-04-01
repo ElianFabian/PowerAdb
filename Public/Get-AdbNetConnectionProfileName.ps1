7fc72a0b883edcbbdbe485bc3623389fbc068dc3
Elián Fabián <elian.fabian.2001@gmail.com>
2025-04-01 21:02:50 +0200

    Fix Get-AdbNetConnectionProfileName verbose

--- Public/Get-AdbNetConnectionProfileName.ps1
+++ Public/Get-AdbNetConnectionProfileName.ps1
@@ -9,7 +9,7 @@ function Get-AdbNetConnectionProfileName {
 
     process {
         foreach ($id in $DeviceId) {
-            Invoke-AdbExpression -DeviceId $id -Command 'shell dumpsys netstats' `
+            Invoke-AdbExpression -DeviceId $id -Command 'shell dumpsys netstats' -Verbose:$VerbosePreference `
             | Select-String -Pattern 'iface=wlan.*networkId="(.+)"'
             | Select-Object -First 1 `
             | ForEach-Object { $_.Matches[0].Groups[1].Value }
