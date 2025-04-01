3b3451d9c2e87c5adb9221319b8c8f3dcb5b6728
Elián Fabián <elian.fabian.2001@gmail.com>
2025-04-01 21:23:24 +0200

    Add Get-AdbRealScreenSize

---
+++ Public/Get-AdbRealScreenSize.ps1
@@ -0,0 +1,24 @@
+function Get-AdbRealScreenSize {
+
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]] $DeviceId
+    )
+
+    foreach ($id in $DeviceId) {
+        Invoke-AdbExpression -DeviceId $id -Command "shell dumpsys display" `
+        | Select-String -Pattern "mStableDisplaySize=Point\((?<width>\d+), (?<height>\d+)\)" `
+        | ForEach-Object { $_.Matches } `
+        | Select-Object -First 1 `
+        | ForEach-Object {
+            $width = [int] $_.Groups["width"].Value
+            $height = [int] $_.Groups["height"].Value
+
+            [PSCustomObject]@{
+                DeviceId       = $id
+                Width  = [int] $width
+                Height = [int] $height
+            }
+        }
+    }
+}
