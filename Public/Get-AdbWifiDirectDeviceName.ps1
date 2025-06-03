1ebcb39677643ff6f4d227da67bff849b2604dd6
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-03 20:03:53 +0200

    Add Get-AdbWifiDirectDeviceName

---
+++ Public/Get-AdbWifiDirectDeviceName.ps1
@@ -0,0 +1,15 @@
+function Get-AdbWifiDirectDeviceName {
+
+    [OutputType([string])]
+    [CmdletBinding()]
+    param (
+        [string] $DeviceId
+    )
+
+    return Invoke-AdbExpression -DeviceId $DeviceId -Command 'shell dumpsys wifi' -Verbose:$VerbosePreference `
+    | Where-Object { $_.StartsWith('wifi_p2p_device_name=')} `
+    | Select-Object -First 1 `
+    | ForEach-Object {
+        $_.Replace('wifi_p2p_device_name=', '')
+    }
+}
