e56970e66bfb456bce5cee06c5d14c6c9c3e0f33
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-06 11:54:07 +0200

    Add NFC releated functions and fix nfc adb help

---
+++ Public/Disable-AdbNfc.ps1
@@ -0,0 +1,17 @@
+function Disable-AdbNfc {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [string] $DeviceId,
+
+        [switch] $IgnoreNfcFeatureCheck
+    )
+
+    Assert-ApiLevel -DeviceId $DeviceId -GreaterThanOrEqualTo 24
+
+    if ($IgnoreNfcFeatureCheck -and -not (Test-AdbFeature -DeviceId $DeviceId -Feature 'android.hardware.nfc' -Verbose:$false)) {
+        Write-Error -Message "Device with id '$DeviceId' does not support NFC." -Category InvalidOperation -ErrorAction Stop
+    }
+
+    Invoke-AdbExpression -DeviceId $DeviceId -Command 'shell svc nfc disable' -Verbose:$VerbosePreference
+}
---
+++ Public/Test-AdbNfc.ps1
@@ -0,0 +1,24 @@
+function Test-AdbNfc {
+
+    [OutputType([bool])]
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [string] $DeviceId,
+
+        [switch] $IgnoreNfcFeatureCheck
+    )
+
+    if ($IgnoreNfcFeatureCheck -and -not (Test-AdbFeature -DeviceId $DeviceId -Feature 'android.hardware.nfc' -Verbose:$false)) {
+        Write-Error -Message "Device with id '$DeviceId' does not support NFC." -Category InvalidOperation -ErrorAction Stop
+    }
+
+    $result = Get-AdbServiceDump -DeviceId $DeviceId -Name 'nfc' | Select-Object -First 1
+
+    switch ($result) {
+        'mState=on' { $true }
+        'mState=off' { $false }
+        default {
+            Write-Error "Unkown state: $_"
+        }
+    }
+}
