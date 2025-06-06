d62d83bbcaf75b9e8d197e324e5e7c0a7556b772
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-06 12:50:48 +0200

    Add TODO in Test-AdbBluetooth

--- Public/Test-AdbBluetooth.ps1
+++ Public/Test-AdbBluetooth.ps1
@@ -13,6 +13,8 @@ function Test-AdbBluetooth {
     }
 
     do {
+        # TODO: There is also the state of BLE_TURNING_ON and BLE_TURNING_OFF, so maybe we could create a differente function
+        # that returns the code for all the states.
         $result = Get-AdbServiceDump -DeviceId $DeviceId -Name bluetooth_manager | Select-Object -Skip 2 -First 1
     }
     while ($result.Contains('TURNING_OFF') -or $result.Contains('TURNING_ON'))
