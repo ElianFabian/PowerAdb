5f8929f0bc71e507de8b3440535fc4c9382792b2
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-06 15:20:23 +0200

    Improve Test-AdbLockScreen performance

--- Public/Test-AdbScreenLocked.ps1
+++ Public/Test-AdbScreenLocked.ps1
@@ -11,7 +11,11 @@ function Test-AdbScreenLocked {
     # https://android.stackexchange.com/a/245699
     $rawData = Get-AdbServiceDump -DeviceId $DeviceId -Name 'deviceidle' -Verbose:$VerbosePreference
 
-    $rawData | Select-String -Pattern 'mScreenLocked=(true|false)' | ForEach-Object {
+    $rawData `
+    | Where-Object { $_.Contains('mScreenLocked=') } `
+    | Select-Object -First 1 `
+    | Select-String -Pattern 'mScreenLocked=(true|false)' `
+    | ForEach-Object {
         $_.Matches.Groups[1].Value -eq 'true'
     }
 }
