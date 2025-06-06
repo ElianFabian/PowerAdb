73d903860980662183dbd971e0fcb869ed43761d
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-06 15:20:35 +0200

    Improve Test-AdbKeyboardOpen performance

--- Public/Test-AdbKeyboardOpen.ps1
+++ Public/Test-AdbKeyboardOpen.ps1
@@ -6,7 +6,13 @@ function Test-AdbKeyboardOpen {
         [string] $DeviceId
     )
 
+    if (Test-AdbScreenLocked -DeviceId $DeviceId) {
+        return $false
+    }
+
     Get-AdbServiceDump -DeviceId $DeviceId -Name 'input_method' -Verbose:$VerbosePreference `
+    | Where-Object { $_.Contains('mInputShown=') } `
+    | Select-Object -First 1 `
     | Select-String -Pattern "mInputShown=(true|false)" `
     | Select-Object -ExpandProperty Matches -First 1 `
     | Select-Object -ExpandProperty Groups -First 2 `
