3ceaeff894a4fd5a4c7432bbb30f39d5784f497d
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-16 15:51:35 +0200

    Add Get-AdbDeviceDetail

---
+++ Public/Get-AdbDeviceDetail.ps1
@@ -0,0 +1,30 @@
+function Get-AdbDeviceDetail {
+
+    [OutputType([PSCustomObject])]
+    [CmdletBinding()]
+    param ()
+
+    Write-Verbose "adb devices -l"
+
+    adb devices -l | Select-Object -Skip 1 -SkipLast 1 `
+    | ForEach-Object {
+        $parts = $_ -split '\s+'
+
+        if ($parts.Count -eq 3) {
+            return [PSCustomObject]@{
+                Id          = $parts[0]
+                State       = $parts[1]
+                TransportId = $parts[2].Replace('transport_id:', '')
+            }
+        }
+
+        return [PSCustomObject]@{
+            Id          = $parts[0]
+            State       = $parts[1]
+            Product     = $parts[2].Replace('product:', '')
+            Model       = $parts[3].Replace('model:', '')
+            Device      = $parts[4].Replace('device:', '')
+            TransportId = $parts[5].Replace('transport_id:', '')
+        }
+    }
+}
