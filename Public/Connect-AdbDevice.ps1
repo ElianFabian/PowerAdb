e0f7db85b2ba6f75506ab8ba0cc0226ded8fbaf3
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-18 19:22:30 +0200

    Remove validate pattern in Connect/Disconnect-AdbDevice
    IpAddress can also be "localhost".

--- Public/Connect-AdbDevice.ps1
+++ Public/Connect-AdbDevice.ps1
@@ -2,7 +2,6 @@ function Connect-AdbDevice {
 
     [CmdletBinding(SupportsShouldProcess)]
     param (
-        [ValidatePattern('\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\b')]
         [Parameter(Mandatory)]
         [string] $IpAddress,
 
