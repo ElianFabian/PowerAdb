4724fc6e47e70114a16a52b8c2b9a7234e78e41c
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-19 16:33:09 +0200

    Improve Start-AdbServer error handling

--- Public/Start-AdbServer.ps1
+++ Public/Start-AdbServer.ps1
@@ -3,5 +3,14 @@ function Start-AdbServer {
     [CmdletBinding(SupportsShouldProcess)]
     param ()
 
-    Invoke-AdbExpression -Command "start-server" -IgnoreExecutionCheck -Verbose:$VerbosePreference
+    try {
+        Invoke-AdbExpression -Command "start-server" -IgnoreExecutionCheck -Verbose:$VerbosePreference
+    }
+    catch [AdbCommandException] {
+        if ($_.Exception.Message.EndsWith('daemon started successfully')) {
+            Write-Host $_.Exception.Message -ForegroundColor Green
+            return
+        }
+        throw $_
+    }
 }
