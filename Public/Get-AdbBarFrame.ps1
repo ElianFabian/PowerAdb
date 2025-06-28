f471d2168d39504156f8dab50bd7ade1a25f1d36
Elián Fabián <elian.fabian.2001@gmail.com>
2025-06-28 10:34:05 +0200

    Add TODO

--- Public/Get-AdbBarFrame.ps1
+++ Public/Get-AdbBarFrame.ps1
@@ -49,3 +49,51 @@ function Get-AdbBarFrame {
 
 $script:StatusBarFramePattern = "m?(type=TYPE_TOP_GESTURES|type=ITYPE_STATUS_BAR|type=statusBars),? m?frame=\[(?<Left>\d+),(?<Top>\d+)\]\[(?<Right>\d+),(?<Bottom>\d+)\]"
 $script:NavigationBarFramePattern = "m?(type=TYPE_BOTTOM_GESTURES|type=ITYPE_NAVIGATION_BAR|type=navigationBars),? m?frame=\[(?<Left>\d+),(?<Top>\d+)\]\[(?<Right>\d+),(?<Bottom>\d+)\]"
+
+
+
+# TODO: We could do something like this to add autocompletion to PSCustomObject members.
+# $FilePathComponents = @'
+# [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+# public class ____________________________FilePathComponents
+# {
+#     public string ContainingFolder { get;set; }
+#     public string FileBaseName { get;set; }
+#     public string FileFullName { get;set; }
+#     public string FileExtension { get;set; }
+#     public string FullPathNoExtension { get;set; }
+#     public string CompletePath { get;set; }
+#     public string ParentFolder { get;set; }
+#     public string UwU { get; }
+# }
+# '@
+
+# Add-Type -TypeDefinition $FilePathComponents -Language CSharp
+
+# function Get-FilePathComponents {
+
+#     [OutputType('____________________________FilePathComponents')]
+
+#     [CmdletBinding()]
+#     param (
+#         [Parameter(Mandatory,Position=0,ValueFromPipeline)]
+#         [String[]]
+#         $Path
+#     )
+
+#     begin {}
+
+#     process {
+#         foreach ($P in $Path) {
+#             $obj = [FilePathComponents]::new()
+#             $obj.ContainingFolder     = Split-Path $P -Parent
+#             $obj.FileBaseName         = Split-Path $P -LeafBase
+#             $obj.FileFullName         = Split-Path $P -Leaf
+#             $obj.FileExtension        = Split-Path $P -Extension
+#             $obj.FullPathNoExtension  = [IO.Path]::Combine((Split-Path $P -Parent),(Split-Path $P -LeafBase))
+#             $obj.CompletePath         = $P
+#             $obj.ParentFolder         = Split-Path (Split-Path $P -Parent) -Parent
+#             $obj
+#         }
+#     }
+# }
